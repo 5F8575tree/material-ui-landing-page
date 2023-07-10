719b7c9cc47e0e50aa3e4437bf01c2beb5bb84a4
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-07-10 10:12:55 +0900

    create navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,38 @@
-import { Typography } from "@mui/material";
-import CustomButton from "./Button";
+import { Box, Button } from "@mui/material";
+
+const pages = [
+  { title: "Tours", arrow: false },
+  { title: "About", arrow: false },
+  { title: "Reviews", arrow: false },
+  { title: "Blog", arrow: false },
+  { title: "Contact", arrow: false },
+];
+
+const styles = {
+  btnLink: {
+    color: "#121212",
+    display: "flex",
+    fontSize: 18,
+    lineHeight: "26px",
+    fontWeight: 500,
+    textTransform: "capitalize",
+    marginRight: "4em",
+  },
+};
 
 const Navbar = () => {
   return (
-    <>
-      <Typography variant="h2">Hello World!</Typography>
-      <CustomButton>My Button</CustomButton>
-    </>
+    <Box
+      sx={{ flexGrow: 1, display: { lg: "flex" }, justifyContent: "flex-end" }}
+    >
+      {pages.map((page, index) => {
+        return (
+          <Button key={index} sx={styles.btnLink}>
+            {page.title}
+          </Button>
+        );
+      })}
+    </Box>
   );
 };
 
