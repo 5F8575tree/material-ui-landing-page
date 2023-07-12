099966d6747dbb7097a3d53780b3a35f5b9568ee
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-07-12 14:21:18 +0900

    complete experiences section

--- components/Experiences/Experiences.js
+++ components/Experiences/Experiences.js
@@ -1,10 +1,9 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 
 const image1 =
-  "https://images.unsplash.com/photo-1496114161697-e76e4b01e4fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
+  "https://images.unsplash.com/photo-1610338730334-ad75b9328d05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=872&q=80";
 const image2 =
   "https://images.unsplash.com/photo-1573102363932-40c3111c5f73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
 const image3 =
@@ -12,36 +11,18 @@ const image3 =
 
 import styles from "./styles";
 
-const links = [
-  "Show all",
-  "Outdoors",
-  "Enoshima",
-  "Shinto",
-  "Buddhism",
-  "Cuisine",
-];
-
 const Experiences = () => {
   return (
     <Box sx={styles.experiences} id="about">
+      <Box sx={styles.background} />
       <Box sx={styles.wrapper}>
-        <Typography variant="h2">Explore the beauty of Kamakura</Typography>
+        <Typography variant="h2">Recent photos from Kamakura</Typography>
         <Typography variant="body1" sx={{ margin: "25px 0" }}>
-          Kamakura was once the capital of all of Japan during the latter middle
-          ages, from 1063 to roughly the middle of the 14th century. It has
-          beaches, history, watersports, fishing, food, and friendly people. For
-          a laidback atmosphere, get away from Tokyo and enjoy this blissful
-          escape for a day or two.
+          Our guides enjoy spending their free time exploring the local area.
+          Kamakura has beaches, history, watersports, fishing, food, and
+          friendly people. For a laidback atmosphere, get away from Tokyo and
+          enjoy this blissful escape for a day or two.
         </Typography>
-        <Box sx={styles.links}>
-          {links.map((item, index) => {
-            return (
-              <Link key={index} sx={styles.link}>
-                {item}
-              </Link>
-            );
-          })}
-        </Box>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
@@ -52,20 +33,24 @@ const Experiences = () => {
             <Box>
               <Box component="img" src={image1} sx={styles.image} />
               <Typography sx={styles.contentTitle}>
-                Unforgetabble Stories
+                Recent spiritual hike
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={6} sm={4} md={4}>
             <Box>
               <Box component="img" src={image2} sx={styles.image} />
-              <Typography sx={styles.contentTitle}>Amazing Sights</Typography>
+              <Typography sx={styles.contentTitle}>
+                Cute Jizo statues
+              </Typography>
             </Box>
           </Grid>
           <Grid item xs={6} sm={4} md={4}>
             <Box>
               <Box component="img" src={image3} sx={styles.image} />
-              <Typography sx={styles.contentTitle}>Wonderful People</Typography>
+              <Typography sx={styles.contentTitle}>
+                Wonderful People, excellent food
+              </Typography>
             </Box>
           </Grid>
         </Grid>
--- components/Experiences/styles.js
+++ components/Experiences/styles.js
@@ -1,10 +1,11 @@
 const styles = {
   experiences: (theme) => ({
-    padding: "44px 20px",
+    padding: "20px",
     display: "flex",
     justifyContent: "center",
     textAlign: "center",
     background: "#F5F5F5",
+    position: "relative",
     [theme.breakpoints.up("sm")]: {
       textAlign: "left",
     },
@@ -14,6 +15,18 @@ const styles = {
   }),
   wrapper: {
     maxWidth: "1240px",
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundImage:
+      'url("https://images.unsplash.com/photo-1678105627738-fa7e5ae584f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8amFwYW4lMjBnZWlzaGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    opacity: 0.1,
   },
   links: (theme) => ({
     flexGrow: 1,
@@ -39,10 +52,9 @@ const styles = {
   contentTitle: (theme) => ({
     fontSize: "14px",
     lineHeight: "17px",
-    marginTop: "12px",
     maxWidth: "402px",
     textAlign: "left",
-    margin: "12px auto 0",
+    margin: "4px auto 0",
     [theme.breakpoints.up("sm")]: {
       fontSize: "24px",
       lineHeight: "29px",
